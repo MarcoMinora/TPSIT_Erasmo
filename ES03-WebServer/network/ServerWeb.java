1a265b2dbff36a5055273275aac6a38e53b92311
MARCO MINORA <80701892+MarcoMinora@users.noreply.github.com>
2022-11-29 11:27:39 +0100

    Update ServerWeb.java

---
+++ ES03-WebServer/network/ServerWeb.java
@@ -0,0 +1,66 @@
+/**
+ * Implementazione di un server web utilizzando la comunicazione tramite socket.
+ * Lettura dati multi riga provenienti dal client
+ * 
+ * from folder network/..
+ * javac network/TcpServer.java; java network.TcpServer 
+ */
+package network;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpServer {
+	public static void main(String[] args) throws Exception {
+		
+		final int SERVER_PORT=8765;
+		String clientMsg = "";
+		
+		try {			 
+			// Creazione del socket sul server e ascolto sulla porta
+			ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+			System.out.println("Server: in ascolto sulla porta " + SERVER_PORT);
+
+			boolean endConn=false;
+			while(!endConn) {
+				// Attesa della connessione con il client
+				System.out.println("Attesa ricezione dati dal client ....................... \n");
+				Socket clientSocket = serverSocket.accept();
+				
+				// Create output stream to write data and input stream to read data from socket
+				DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());	
+				BufferedReader inStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	
+				// ---------------------------------------------------------
+				//Lettura dati dal client un righa alla volta   
+				while ((clientMsg=inStream.readLine()).length() != 0) {
+					System.out.println(clientMsg);	
+				}  
+				// Elaborare qui i dati ricevuti dal client 
+				// ---------------------------------------------------------
+
+				//Invio dei dati su stream di rete al client
+				clientMsg = "HTTP/1.1 200 OK\r\n";
+				//clientMsg += "Connection: close\r\n";
+				//clientMsg += "Content-Type: text/plain\r\n";
+				clientMsg += "\r\n";
+				clientMsg += "Saluti dal web server Java";
+				outStream.write(clientMsg.getBytes());
+				outStream.flush();
+
+				System.out.println("\n....................... Fine ricezione dati\n");
+				// Close resources
+				clientSocket.close();
+				inStream.close();
+				outStream.close();
+			}
+
+			// Close resources
+			serverSocket.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
